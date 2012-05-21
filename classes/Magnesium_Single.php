25136028bf1f64b57aca4e971f360aa78651a237
Phillip Whittlesea <pw.github@thega.me.uk>
2012-05-21 12:26:09 +0100

    Comment Single class

--- classes/Magnesium_Single.php
+++ classes/Magnesium_Single.php
@@ -1,45 +1,83 @@
 <?php
 /*
+ * Subject single
+ *
  * Author: Phillip Whittlesea <pw.github@thega.me.uk>
  * Date: 12/05/2012
  */
- 
-class Magnesium_Single { 
 
-    private $url;
-    private $magnesium;
+class Magnesium_Single {
 
-    // private constructor function 
-    function __construct($magnesium = null, $uri = null) { 
+    // The identifier for the Subject
+    private static $uri;
+    // Link to Magnesium master object
+    private static $magnesium;
+
+    /**
+     * __construct
+     * Private constructor function
+     *
+     * @param string $magnesium Master object
+     * @param string $uri new Subject id
+     */
+    function __construct($magnesium = null, $uri = null) {
         $this->magnesium = $magnesium;
-        $this->url = $uri;
-    } 
-    
-    public function rels($link = '') {
-        $query = "SELECT * WHERE { <".$this->url."> ".$link." ?b . }";
+        $this->uri = $uri;
+    }
+
+    /**
+     * rels
+     * Fetch list of objects using predicate
+     *
+     * @param string $predicate predicate to use
+     *
+     * @return array Magnesium_Single objects
+     */
+    public function rels($predicate = '') {
+        $query = "SELECT ?o WHERE { <".$this->uri."> ".$predicate." ?o . }";
 
         $o = array();
         if ($rows = $this->magnesium->query($query)) {
             foreach ($rows as $row) {
-                array_push( $o , $this->magnesium->get( $row['b'] ));
+                array_push( $o , $this->magnesium->get( $row['o'] ));
             }
         }
         return $o;
     }
 
+    /**
+     * rel
+     * Fetch object using predicate
+     *
+     * @param string $predicate predicate to use
+     *
+     * @return Magnesium_Single object
+     */
     public function rel($link = '') {
         $rels = $this->rels($link);
         if( count($rels) > 0 ) return $rels[0];
         return null;
     }
 
+    /**
+     * type
+     * Fetch type of object
+     *
+     * @return Magnesium_Single type
+     */
     public function type() {
         return $this->rel( "rdf:type" );
     }
 
+    /**
+     * type
+     * Fetch label of object
+     *
+     * @return String rdfs:label of object
+     */
     public function __toString() {
         $l = $this->rel( "rdfs:label" );
         if($l) return (string) $l;
-        return (string) $this->url;
+        return (string) $this->uri;
     }
 }
